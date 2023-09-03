8faa115c27ff726a841bba711f738cc9d16d1068
Abena9 <140721676+Abena9@users.noreply.github.com>
2023-09-03 23:47:31 +0300

    see
    see

---
+++ evangadi/X Delivery/script.js
@@ -0,0 +1,18 @@
+
+const var2 = document.querySelector(".top-header");
+var var1 = document.querySelectorAll(".nav-bar");
+// const var let
+// console log  = print
+// document.querySelector()
+console.log(var1)
+// console.log("text");
+// console.log("adgadfad")
+
+function print(){
+    // remove add toggle
+    // classlist targets all classes
+    var2.classList.toggle("row")
+    var1.style.display = "flex"
+    var1.style.flexDirection = "column"
+    console.log("text");
+}
